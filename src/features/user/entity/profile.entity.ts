d466ae8448c123eb9a9925dc1619e9b9005adfc8
Air <air@sarzeez-2.local>
2023-11-10 22:06:27 +0500

    notes enpdoint

--- src/features/user/entity/profile.entity.ts
+++ src/features/user/entity/profile.entity.ts
@@ -1,5 +1,12 @@
-import { Column, Entity, OneToOne, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  OneToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 import { User } from './user.entity';
+import { File } from '@/features/file/entity/file.entity';
 
 export enum Gender {
   GENDER_MALE = 'male',
@@ -20,10 +27,11 @@ export class Profile {
   gender: Gender;
 
   @Column({ default: null })
-  photo: string;
-
-  @Column({ default: null })
   age: number;
+
+  @OneToOne(() => File, (file) => file.profile, { eager: true })
+  @JoinColumn({ name: 'file_id' })
+  avatar: File;
 
   @OneToOne(() => User, (user) => user.profile)
   user: User;
